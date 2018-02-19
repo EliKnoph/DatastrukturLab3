2706f911d9c146c7c886792dd51c17a3633a60c0
Rudolf <eli.knoph@gmail.com>
2018-02-19 14:08:18 +0100

    all the classes

---
+++ DirectedGraph.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+import java.util.*;
+
+public class DirectedGraph<E extends Edge> {
+
+	public Element element = new Element(node);
+
+	private PriorityQueue <Element> q = new PriorityQueue<>();
+
+
+	public DirectedGraph(int noOfNodes) {
+		;
+	}
+
+	public void addEdge(E e) {
+		;
+	}
+
+	public Iterator<E> shortestPath(int from, int to) {
+		return null;
+	}
+		
+	public Iterator<E> minimumSpanningTree() {
+		return null;
+	}
+
+
+}
+  
---
+++ Element.java
@@ -0,0 +1,49 @@
+import java.util.PriorityQueue;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.Iterator;
+//import java.io.FileNotFoundException;
+import java.awt.Point;
+import java.awt.Color;
+import java.text.DecimalFormat;
+
+public class Element {
+
+
+    private static final double infinity = 1.0E300;
+
+
+    private Node node;
+    private int cost;
+    private Node[] path;
+
+    public Element(){
+
+        this.node = node;
+        this.cost = cost;
+        this.path = path;
+
+    }
+
+    private static class Node  implements Comparable<Node>{
+
+        Point p = null;
+        double imp = infinity;  //importance of the point
+        int nbr = -1;   // original position in list, only for debug
+
+        private Node next = null; // The link to the next node.
+        private Node prev = null; // The link to the previous node.
+
+        private Node( Point p, int nbr ) { // nbr stores the original position in list
+            this.p = p;
+            this.nbr = nbr;
+        }
+
+        public int compareTo( Node n ) {
+            double diff= imp-n.imp;
+            if (diff<0) { return -1;
+            } else if (diff > 0) { return +1;
+            } else { return 0; }
+        }
+    }
+}
